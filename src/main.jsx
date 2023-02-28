34e65be1ae4b23592a5e5d9098237ba40293fbe0
Lucas Takeo Mori <morilucasaluno@gmail.com>
2023-02-28 19:47:58 -0300

    'CHANGES'

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import {Home} from './Pages/Home';
+
+import './style/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
 )
